e1176b99a152f484e1fd4f646067122a206d44b8
Brenna Switzer <brenna.switzer@gmail.com>
2020-12-04 21:52:22 -0800

    day four, part two 2020

--- app/src/main/kotlin/aoc/y2020/DayFour.kt
+++ app/src/main/kotlin/aoc/y2020/DayFour.kt
@@ -1,16 +1,77 @@
 package aoc.y2020
 
 data class Passport(val attributes: Map<String,String>) {
-//    val byr:String = "", // (Birth Year)
-//    val iyr:String = "", // (Issue Year)
-//    val eyr:String = "", // (Expiration Year)
-//    val hgt:String = "", // (Height)
-//    val hcl:String = "", // (Hair Color)
-//    val ecl:String = "", // (Eye Color)
-//    val pid:String = "", // (Passport ID)
-//    val cid:String = ""  // (Country ID)
 
     private val required = listOf("byr","iyr","eyr","hgt","hcl","ecl","pid")
+
+    //    byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    fun isValidByr(v:String) : Boolean {
+        return v.length == 4 && v.toInt() in 1920..2002
+    }
+
+    //    iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    fun isValidIyr(v:String):Boolean {
+        return v.length == 4 && v.toInt() in 2010..2020
+    }
+
+    //    eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    fun isValidEyr(v:String):Boolean {
+        return v.length == 4 && v.toInt() in 2020..2030
+    }
+
+    //    hgt (Height) - a number followed by either cm or in:
+    //      If cm, the number must be at least 150 and at most 193.
+    //      If in, the number must be at least 59 and at most 76.
+    fun isValidHgt(v: String) : Boolean {
+        val len = v.length - 1
+        val unit = v.substring(len-1..len)
+        val num = v.substring(0 until len-1)
+        return when (unit) {
+            "cm" -> { num.toInt() in 150..193 }
+            "in" -> { num.toInt() in 59..76 }
+            else -> { false }
+        }
+    }
+
+    //    hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    fun isValidHcl(v:String):Boolean {
+        val test = v
+                .trim()
+                .substring(v.indices)
+                .split("")
+                .slice(1..v.length)
+        return test[0] == "#" && test.all { c -> c.matches("#|[a-f]|[0-9]".toRegex()) }
+    }
+
+    //    ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    fun isValidEcl(v:String):Boolean {
+        return v.matches("amb|blu|brn|gry|grn|hzl|oth".toRegex())
+    }
+
+    //    pid (Passport ID) - a nine-digit number, including leading zeroes.
+    fun isValidPid(v:String):Boolean {
+        return v.matches("\\d{9}".toRegex())
+    }
+//    cid (Country ID) - ignored, missing or not.
+
+
+    fun isValid() : Boolean {
+        if (!isComplete()) return false
+        val valid = attributes.entries.map { (k,v) ->
+            when(k) {
+                "byr" -> isValidByr(v)
+                "iyr" -> isValidIyr(v)
+                "eyr" -> isValidEyr(v)
+                "hgt" -> isValidHgt(v)
+                "hcl" -> isValidHcl(v)
+                "ecl" -> isValidEcl(v)
+                "pid" -> isValidPid(v)
+                "cid" -> true
+                else -> false
+            }
+        }
+        return valid.all { it }
+    }
 
     fun isComplete():Boolean {
         val valid = required.map { att ->
@@ -50,6 +111,16 @@ class DayFour2020(val input: String) {
         }
         return count
     }
+
+    fun partTwo():Int {
+        var count = 0
+        passports.forEach {
+            if(it.isValid()) {
+                count++
+            }
+        }
+        return count
+    }
 }
 
 fun main(args: Array<String>) {
@@ -57,4 +128,5 @@ fun main(args: Array<String>) {
     val input = cl.readText()
     val solver = DayFour2020(input)
     println(solver.partOne()) //210
+    println(solver.partTwo()) //131
 }
--- app/src/test/kotlin/aoc/y2020/DayFourTest.kt
+++ app/src/test/kotlin/aoc/y2020/DayFourTest.kt
@@ -2,6 +2,8 @@ package aoc.y2020
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlin.test.assertFalse
 
 class DayFour2020Test {
     private val input = """
@@ -20,9 +22,79 @@ class DayFour2020Test {
         iyr:2011 ecl:brn hgt:59in
         """.trimIndent()
 
+    private val invalidInput = """
+        eyr:1972 cid:100
+        hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+        iyr:2019
+        hcl:#602927 eyr:1967 hgt:170cm
+        ecl:grn pid:012533040 byr:1946
+
+        hcl:dab227 iyr:2012
+        ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+        hgt:59cm ecl:zzz
+        eyr:2038 hcl:74454a iyr:2023
+        pid:3556412378 byr:2007
+    """.trimIndent()
+
+    private val validInput = """
+        pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+        hcl:#623a2f
+
+        eyr:2029 ecl:blu cid:129 byr:1989
+        iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+        hcl:#888785
+        hgt:164cm byr:2001 iyr:2015 cid:88
+        pid:545766238 ecl:hzl
+        eyr:2022
+
+        iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
+    """.trimIndent()
+
+    private val solver = DayFour2020(input)
+    private val passport = Passport(mapOf())
+
     @Test fun testEx1() {
-        val solver = DayFour2020(input)
         assertEquals(2, solver.partOne())
     }
 
+    @Test fun testPartTwoInvalid() {
+        val solver2 = DayFour2020(invalidInput)
+        assertEquals(0, solver2.partTwo())
+    }
+
+    @Test fun testPartTwoValid() {
+        val solver2 = DayFour2020(validInput)
+        assertEquals(4, solver2.partTwo())
+    }
+
+    @Test fun testByrValid() {
+        assertTrue(passport.isValidByr("2002"))
+        assertFalse(passport.isValidByr("2003"))
+    }
+
+    @Test fun testHgt() {
+        assertTrue(passport.isValidHgt("60in"))
+        assertTrue(passport.isValidHgt("190cm"))
+        assertFalse(passport.isValidHgt("190in"))
+        assertFalse(passport.isValidHgt("190"))
+    }
+
+    @Test fun testHcl() {
+        assertTrue(passport.isValidHcl("#123abc"))
+        assertFalse(passport.isValidHcl("#123abz"))
+        assertFalse(passport.isValidHcl("123abc"))
+    }
+
+    @Test fun testEcl() {
+        assertTrue(passport.isValidEcl("brn"))
+        assertFalse(passport.isValidEcl("wat"))
+    }
+
+    @Test fun testPid() {
+        assertTrue(passport.isValidPid("000000001"))
+        assertFalse(passport.isValidPid("0123456789"))
+    }
 }
