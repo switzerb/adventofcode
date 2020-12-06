bfb4bd99c96ff414bfd8bb90786a30006dca89ac
Brenna Switzer <brenna.switzer@gmail.com>
2020-12-05 21:17:05 -0800

    wip

---
+++ app/src/main/kotlin/aoc/y2020/DaySix.kt
@@ -0,0 +1,7 @@
+package aoc.y2020
+
+class DaySix2020 {}
+
+fun main(args: Array<String>) {
+    println("Hello")
+}
---
+++ app/src/test/kotlin/aoc/y2020/DaySixTest.kt
@@ -0,0 +1,12 @@
+package aoc.y2020
+
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class DaySix2020Test {
+    @Test
+    fun ex1() {
+        assertTrue(true)
+    }
+}
