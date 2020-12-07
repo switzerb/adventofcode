08f2ae0ac235976e1a45e72c5ebd0593ac16a8a5
Brenna Switzer <brenna.switzer@gmail.com>
2020-12-07 08:21:34 -0800

    Setup day seven

---
+++ app/src/main/kotlin/aoc/y2020/DaySeven.kt
@@ -0,0 +1,18 @@
+package aoc.y2020
+
+class DaySeven2020(val input: String) {
+
+    private fun parser(): List<String> {
+        return input.lines()
+    }
+
+    fun partOne():Int {
+        val rules = parser()
+        println(rules)
+        return 0
+    }
+}
+
+fun main(args: Array<String>) {
+    println("hello world")
+}
