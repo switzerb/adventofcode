5c96d7014ef111d79606873ea8701ec1787455dc
Brenna Switzer <brenna.switzer@gmail.com>
2020-12-12 10:08:21 -0800

    day nine, part one and two

---
+++ app/src/test/kotlin/aoc/y2020/DayNineTest.kt
@@ -0,0 +1,43 @@
+package aoc.y2020
+
+import aoc.lib.Resources.rawAsLong
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class DayNine2020Test {
+
+    val input = """
+        35
+        20
+        15
+        25
+        47
+        40
+        62
+        55
+        65
+        95
+        102
+        117
+        150
+        182
+        127
+        219
+        299
+        277
+        309
+        576
+    """.trimIndent()
+
+    @Test
+    fun part1() {
+        val solver = DayNine2020(rawAsLong(input), 5)
+        assertEquals(127, solver.partOne())
+    }
+
+    @Test
+    fun part2() {
+        val solver = DayNine2020(rawAsLong(input), 5)
+        assertEquals(62, solver.partTwo())
+    }
+}
