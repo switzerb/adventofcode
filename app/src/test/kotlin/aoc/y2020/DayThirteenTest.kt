e19add7bbf6bf0e802dab943a1c05dcf4fcdd2c7
Brenna Switzer <brenna.switzer@gmail.com>
2020-12-20 09:22:33 -0800

    day thirteen, part one 2020

---
+++ app/src/test/kotlin/aoc/y2020/DayThirteenTest.kt
@@ -0,0 +1,17 @@
+package aoc.y2020
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class DayThirteen2020Test {
+
+    private val depart = 939
+    val input = listOf("7","13","x","x","59","x","31","19")
+
+    val solver = DayThirteen2020(depart,input)
+
+    @Test
+    fun ex1() {
+        assertEquals(295, solver.partOne())
+    }
+}
