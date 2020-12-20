cbb7526480c07e7bf48556ad0669b961d8499815
Brenna Switzer <brenna.switzer@gmail.com>
2020-12-20 10:11:14 -0800

    day thirteen, part one 2020

--- app/src/main/kotlin/aoc/y2020/DayThirteen.kt
+++ app/src/main/kotlin/aoc/y2020/DayThirteen.kt
@@ -2,24 +2,20 @@ package aoc.y2020
 
 import aoc.lib.Resources.fileAsString
 
-class DayThirteen2020(val depart: Int, val input: List<String>) {
+class DayThirteen2020(val timestamp: Int, val input: List<String>) {
 
-    fun partOne(): Int {
-        val schedule = input.filter { it != "x" }.map { it.toInt() }
-        (0..depart+100).fold(emptyList<Int>()) { a, tick ->
-            val earliest = a.find { bus -> bus >= depart }
-            if(earliest != null) {
-                val bus = schedule[a.indexOf(earliest)]
-                return (earliest - depart) * bus
-            }
-            schedule.map { it * tick }
-        }
-        return 0
+    fun partOne(): Int? {
+        val schedule = input.filter { it != "x" }.map { it.toInt() }.sorted()
+        val whatever = schedule
+            .map { it - (timestamp % it) }
+            .zip(schedule)
+            .minBy { it.first }
+        return whatever?.first?.times(whatever.second)
     }
 }
 
 fun main(args: Array<String>) {
     val input = fileAsString("day13_2020.txt").trim().split(",")
     val solver = DayThirteen2020(1011416, input)
-    println(solver.partOne()) //642255
+    println(solver.partOne()) //4135
 }
