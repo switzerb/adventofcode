e2d3c0116db454deb6e5b38c289bcf7831c11377
Brenna Switzer <brenna.switzer@gmail.com>
2020-12-25 15:08:56 -0800

    day 15, part two 2020

--- app/src/main/kotlin/aoc/y2020/Day15.kt
+++ app/src/main/kotlin/aoc/y2020/Day15.kt
@@ -1,39 +1,52 @@
 package aoc.y2020
 
-data class Turn(val turn: Int, val number: Int)
+import kotlin.system.measureTimeMillis
 
 class Day152020(val input: List<Int>) {
 
     private val start = input.toMutableList()
-    private val said = mutableListOf<Turn>()
+    private val said = mutableMapOf<Int, Int>()
+    private var previous: Int = -1;
 
-    fun say(turn: Turn): Boolean = said.add(turn)
+    fun say(number: Int, turn: Int): Unit { said[number] = turn }
 
-    fun getPrev(idx: Int) = said[idx-1].number
-
-    fun dequeue(idx: Int) {
-        say(Turn(idx + 1,start[0]) )
-        start.removeAt(0)
-    }
-
-    fun getNextNumber(idx: Int) : Turn {
-        val repeated = said.filter { it.number == getPrev(idx) }
-        val len = repeated.size
-        return if(len == 1) {
-            Turn(idx+1,0)
-        } else {
-            Turn(idx+1,repeated[len - 1].turn - repeated[len - 2].turn)
+    fun play(target: Int) : Int {
+        (1..target).map { turn ->
+            if(start.isNotEmpty()){
+                previous = start[0]
+                start.removeAt(0)
+                if(start.size >= 1) {
+                    say(previous, turn)
+                }
+            } else {
+                val lastSaid = said[previous]
+                say(previous,turn - 1)
+                previous = if(lastSaid == null) 0 else (turn - 1) - lastSaid
+            }
         }
+        return previous
 
     }
-
     fun partOne(): Int {
-        (0..2020).map { idx -> if(start.isNotEmpty())  dequeue(idx)  else  say(getNextNumber(idx)) }
-        return said[2019].number
+        return play(2020)
     }
+
+    fun partTwo(): Int {
+        return play(30000000)
+    }
+
 }
 
 fun main(args: Array<String>) {
   val solver = Day152020(listOf(2,1,10,11,0,6))
     println(solver.partOne()) //232
+    val timeInMillis = measureTimeMillis {
+        println(solver.partTwo())
+    }
+    println(timeInMillis)
+
 }
+
+// 203ms    10,000
+// 771ms    100,000
+// 1525.22s 1,000,000
