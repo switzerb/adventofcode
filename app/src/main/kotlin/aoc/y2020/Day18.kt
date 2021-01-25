a0b4af9af07194185b3cb183dc4440b0f054b434
Brenna Switzer <brenna.switzer@gmail.com>
2021-01-25 10:35:28 -0800

    part two, day 18 2020

--- app/src/main/kotlin/aoc/y2020/Day18.kt
+++ app/src/main/kotlin/aoc/y2020/Day18.kt
@@ -15,24 +15,52 @@ class Day18(val input: List<String>) {
                 '*' -> op = next
                 '(' -> nums.add(eval(exp))
                 ')' -> break
-                else -> nums.add(next.toString().toLong())
+                else -> {
+                    if(!next.isDigit()) throw IllegalArgumentException("$next cannot be evaluated")
+                    nums += next.toString().toLong()
+                }
             }
             if(nums.size == 2) {
 //                println(nums)
                 val (a,b) = nums
-                nums.clear()
-                nums.plusAssign(if (op == '+') a + b else a * b)
+                with(nums) {
+                    clear()
+                    plusAssign(if (op == '+') a + b else a * b)
+                }
             }
         }
         return nums.first()
     }
 
+    private fun evalWithPrecedence(exp: CharIterator) : Long {
+        val products = mutableListOf<Long>()
+        var adds: Long = 0
+        while (exp.hasNext()) {
+            val next = exp.nextChar()
+            when {
+                next == '(' -> adds += evalWithPrecedence(exp)
+                next == ')' -> break
+                next == '*' -> {
+                    products += adds
+                    adds = 0
+                }
+                next.isDigit() -> adds += next.toString().toLong()
+            }
+        }
+        return (products + adds).reduce { a, it -> a * it }
+    }
+
     fun partOne(): Long {
         return expressions.sumOf { exp -> eval(exp.iterator()) }
+    }
+
+    fun partTwo() : Long {
+        return expressions.sumOf { exp -> evalWithPrecedence(exp.iterator()) }
     }
 }
 
 fun main(args :Array<String>) {
     val solver = Day18(fileAsList("day18_2020.txt"))
-    println(solver.partOne())
+    println(solver.partOne()) //11076907812171
+    println(solver.partTwo()) //283729053022731
 }
--- app/src/test/kotlin/aoc/y2020/Day18Test.kt
+++ app/src/test/kotlin/aoc/y2020/Day18Test.kt
@@ -6,65 +6,76 @@ import kotlin.test.assertEquals
 
 class Day18Test {
 
-    private val example1 = """
-        1 + 2 * 3 + 4 * 5 + 6
-    """.trimIndent()
-
-    private val example2 = """
-        1 + (2 * 3) + (4 * (5 + 6))
-    """.trimIndent()
-
-    private val example3 = """
-        2 * 3 + (4 * 5)
-    """.trimIndent()
-
-    private val example4 = """
-        5 + (8 * 3 + 9 + 3 * 4 * 3)
-    """.trimIndent()
-
-    private val example5 = """
-        5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
-    """.trimIndent()
-
-    private val example6 = """
-        ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2
-    """.trimIndent()
-
-
     @Test
     fun testOrder() {
-        val solver = Day18(rawAsList(example1))
+        val solver = Day18(rawAsList("1 + 2 * 3 + 4 * 5 + 6"))
         assertEquals(71, solver.partOne())
     }
 
     @Test
     fun testWithParens() {
-        val solver = Day18(rawAsList(example2))
+        val solver = Day18(rawAsList("1 + (2 * 3) + (4 * (5 + 6))"))
         assertEquals(51, solver.partOne())
     }
 
     @Test
     fun testMore3() {
-        val solver = Day18(rawAsList(example3))
+        val solver = Day18(rawAsList("2 * 3 + (4 * 5)"))
         assertEquals(26, solver.partOne())
     }
 
     @Test
     fun testMore4() {
-        val solver = Day18(rawAsList(example4))
+        val solver = Day18(rawAsList("5 + (8 * 3 + 9 + 3 * 4 * 3)"))
         assertEquals(437, solver.partOne())
     }
 
     @Test
     fun testMore5() {
-        val solver = Day18(rawAsList(example5))
+        val solver = Day18(rawAsList("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"))
         assertEquals(12240, solver.partOne())
     }
 
     @Test
     fun testMore6() {
-        val solver = Day18(rawAsList(example6))
+        val solver = Day18(rawAsList("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))
         assertEquals(13632, solver.partOne())
+    }
+
+    @Test
+    fun testWithPrecedence() {
+        val solver = Day18(rawAsList("1 + 2 * 3 + 4 * 5 + 6"))
+        assertEquals(231, solver.partTwo())
+    }
+
+    @Test
+    fun testWithPMore1() {
+        val solver = Day18(rawAsList("1 + (2 * 3) + (4 * (5 + 6))"))
+        assertEquals(51, solver.partTwo())
+    }
+
+    @Test
+    fun testWithPMore2() {
+        val solver = Day18(rawAsList("2 * 3 + (4 * 5)"))
+        assertEquals(46, solver.partTwo())
+    }
+
+    @Test
+    fun testWithPMore3() {
+        val solver = Day18(rawAsList("5 + (8 * 3 + 9 + 3 * 4 * 3)"))
+        assertEquals(1445, solver.partTwo())
+    }
+
+    @Test
+    fun testWithPMore4() {
+        val solver = Day18(rawAsList("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"))
+        assertEquals(669060, solver.partTwo())
+    }
+
+    @Test
+    fun testWithPMore5() {
+        val solver = Day18(rawAsList("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"))
+        assertEquals(23340, solver.partTwo())
     }
 
 }
