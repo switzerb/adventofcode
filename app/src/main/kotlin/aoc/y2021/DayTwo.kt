d0e58f5ad0d37471d884fd1451caef504f0be7b6
Brenna Switzer <bswitzer@brex.com>
2021-12-01 21:47:26 -0800

    day 2 2021 part one

--- app/src/main/kotlin/aoc/y2021/DayTwo.kt
+++ app/src/main/kotlin/aoc/y2021/DayTwo.kt
@@ -1,17 +1,39 @@
 package aoc.y2021
-import aoc.lib.Resources.fileAsString
+import aoc.lib.Resources.fileAsList
 
-class DayTwo(private val input: String) {
+data class Step(val command: String, val amount: Int)
 
-    fun partOne(): String {
-        return input
+class DayTwo(private val input: List<String>) {
+
+    fun parse(): List<Step> {
+        return input.map { line ->
+            line.split(" ")
+        }.map { step ->
+            Step(command = step[0], amount = step[1].toInt())
+        }
+    }
+
+    fun partOne(): Int {
+        var position = 0
+        var depth = 0
+
+        val parsedInput = parse()
+
+        parsedInput.map { step ->
+            when (step.command) {
+                "forward" -> position += step.amount
+                "down" -> depth += step.amount
+                "up" -> depth -= step.amount
+            }
+        }
+        return position * depth
     }
 
     fun partTwo() {}
 }
 
 fun main(args: Array<String>) {
-    val input = fileAsString("day02_2021.txt")
+    val input = fileAsList("day02_2021.txt")
     val solver = DayTwo(input)
-    println(solver.partOne())
+    println(solver.partOne()) // 1698735
 }
--- app/src/test/kotlin/aoc/y2021/DayTwoTest.kt
+++ app/src/test/kotlin/aoc/y2021/DayTwoTest.kt
@@ -1,12 +1,23 @@
 package aoc.y2021
 
+import aoc.lib.Resources.rawAsList
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class DayTwoTest {
 
+    val input = """
+        forward 5
+        down 5
+        forward 8
+        up 3
+        down 8
+        forward 2
+    """.trimIndent()
+
     @Test
     fun test() {
-        assertEquals(true, true)
+        val solver = DayTwo(rawAsList(input))
+        assertEquals(150, solver.partOne())
     }
 }
