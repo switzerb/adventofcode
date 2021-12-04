9ee4dddec0c2ac8c0979cdd88061710e64bf8e21
Brenna Switzer <bswitzer@brex.com>
2021-12-03 23:28:05 -0800

    day 3 part one 2021

--- app/src/main/kotlin/aoc/y2021/DayThree.kt
+++ app/src/main/kotlin/aoc/y2021/DayThree.kt
@@ -1,17 +1,40 @@
 package aoc.y2021
-import aoc.lib.Resources.fileAsString
+import aoc.lib.Resources.fileAsList
 
-class DayThree(private val input: String) {
+class DayThree(private val input: List<String>, val width: Int = 12) {
 
-    fun partOne(): String {
-       return input
+    val n = width - 1
+
+    fun getRate(compare: String): String {
+        val rate = mutableListOf<Char>()
+        (0..n).map { idx ->
+            val histogram = input
+                .groupingBy { it[idx] }
+                .eachCount()
+
+            val max = histogram.maxByOrNull { it.value }
+            val min = histogram.minByOrNull { it.value }
+
+            when (compare) {
+                "more" -> rate.add(max?.key!!)
+                "less" -> rate.add(min?.key!!)
+                else -> null
+            }
+        }
+        return rate.joinToString("")
+    }
+
+    fun partOne(): Int {
+        val gammaDecimal = getRate("more").toInt(2)
+        val epsilonDecimal = getRate("less").toInt(2)
+        return gammaDecimal * epsilonDecimal
     }
 
     fun partTwo() {}
 }
 
 fun main(args: Array<String>) {
-    val input = fileAsString("day03_2021.txt")
+    val input = fileAsList("day03_2021.txt")
     val solver = DayThree(input)
-    println(solver.partOne())
+    println(solver.partOne()) // 2640986
 }
--- app/src/test/kotlin/aoc/y2021/DayThreeTest.kt
+++ app/src/test/kotlin/aoc/y2021/DayThreeTest.kt
@@ -1,12 +1,41 @@
 package aoc.y2021
 
-import org.junit.Test
+import aoc.lib.Resources.fileAsList
+import aoc.lib.Resources.rawAsList
 import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class DayThreeTest {
 
+    val input = """
+        00100
+        11110
+        10110
+        10111
+        10101
+        01111
+        00111
+        11100
+        10000
+        11001
+        00010
+        01010
+    """.trimIndent()
+
     @Test
-    fun test() {
-        assertEquals(true, true)
+    fun testEx1() {
+        val solver = DayThree(
+            input = rawAsList(input),
+            width = 5
+        )
+        assertEquals(solver.partOne(), 198)
+    }
+
+    @Test
+    fun testDay1Actual() {
+        val solver = DayThree(
+            input = fileAsList("day03_2021.txt")
+        )
+        assertEquals(solver.partOne(), 2640986)
     }
 }
