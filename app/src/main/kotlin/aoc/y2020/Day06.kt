6ba8749db793281ab7563e69cbe275c79c2892b7
Brenna Switzer <bswitzer@brex.com>
2021-12-05 13:45:05 -0800

    ktlint

--- app/src/main/kotlin/aoc/y2020/Day06.kt
+++ app/src/main/kotlin/aoc/y2020/Day06.kt
@@ -6,35 +6,35 @@ class DaySix2020(val input: String) {
 
     fun partOne(): Int {
         return input
-                .trim()
-                .split("\n\n".toRegex())
-                .asSequence()
-                .map { it.replace("\n","")}
-                .map { it.toSet() }
-                .map { it.joinToString("")}
-                .map { it.length }
-                .sum()
+            .trim()
+            .split("\n\n".toRegex())
+            .asSequence()
+            .map { it.replace("\n", "") }
+            .map { it.toSet() }
+            .map { it.joinToString("") }
+            .map { it.length }
+            .sum()
     }
 
-    private fun histogram(answers: List<String>) :Int {
-        val hist = mutableMapOf<Char,Int>()
+    private fun histogram(answers: List<String>): Int {
+        val hist = mutableMapOf<Char, Int>()
         answers.forEach { person ->
             person.forEach {
-                hist[it] = hist.getOrDefault(it,0) + 1
+                hist[it] = hist.getOrDefault(it, 0) + 1
             }
         }
         return hist
-                .filterValues { it == answers.size }
-                .size
+            .filterValues { it == answers.size }
+            .size
     }
 
     fun partTwo(): Int {
         return input
-                .trim()
-                .split("\n\n".toRegex())
-                .map { it.split("\n") }
-                .map { histogram(it)}
-                .sum()
+            .trim()
+            .split("\n\n".toRegex())
+            .map { it.split("\n") }
+            .map { histogram(it) }
+            .sum()
     }
 }
 
@@ -43,5 +43,4 @@ fun main(args: Array<String>) {
     val solver = DaySix2020(input)
     println(solver.partOne()) // 7110
     println(solver.partTwo()) // 3628
-
 }
