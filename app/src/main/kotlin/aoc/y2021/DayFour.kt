eb1415230e2dc0365e3cd18806594fa04b5528e1
Brenna Switzer <bswitzer@brex.com>
2021-12-04 19:59:16 -0800

    day 4 crap

--- app/src/main/kotlin/aoc/y2021/DayFour.kt
+++ app/src/main/kotlin/aoc/y2021/DayFour.kt
@@ -1,17 +1,61 @@
 package aoc.y2021
+import aoc.lib.Resources.fileAsList
 import aoc.lib.Resources.fileAsString
 
-class DayFour(private val input: String) {
+typealias BingoBoard = List<List<Int>>
 
-    fun partOne(): String {
-       return input
+class DayFour(input: List<String>) {
+
+    private val draws: List<Int> = input.first().split(",").map { it.toInt() }
+    private val boards: Set<BingoBoard> = parseInput(input)
+
+    fun partOne(): Int {
+        val drawn = draws.take(4).toMutableSet()
+        return draws.drop(4).firstNotNullOf { draw ->
+            drawn += draw
+            boards.firstOrNull { it.isWinner(drawn) }?.let { winner ->
+                draw * winner.sumUnmarked(drawn)
+            }
+        }
     }
 
-    fun partTwo() {}
+    fun partTwo(): Int {
+        val drawn = draws.toMutableSet()
+        return draws.reversed().firstNotNullOf { draw ->
+            drawn -= draw
+            boards.firstOrNull { !it.isWinner(drawn) }?.let { winner ->
+                draw * (winner.sumUnmarked(drawn) - draw)
+            }
+        }
+    }
+
+    private fun BingoBoard.isWinner(draws: Set<Int>) =
+        this.any { row -> row.all { it in draws } } ||
+            (0..4).any { col -> this.all { row -> row[col] in draws } }
+
+    private fun BingoBoard.sumUnmarked(draws: Set<Int>): Int =
+        this.sumOf { row ->
+            row.filterNot { it in draws }.sum()
+        }
+
+    private fun parseInput(input: List<String>): Set<BingoBoard> =
+        input
+            .asSequence()
+            .drop(1)
+            .filter { it.isNotEmpty() }
+            .chunked(5)
+            .map { parseSingleBoard(it) }
+            .toSet()
+
+    private fun parseSingleBoard(input: List<String>): BingoBoard =
+        input.map { row ->
+            row.split(" ").filter { it.isNotEmpty() }.map { it.toInt() }
+        }
 }
 
 fun main(args: Array<String>) {
-    val input = fileAsString("day04_2021.txt")
+    val input = fileAsList("day04_2021.txt")
     val solver = DayFour(input)
     println(solver.partOne())
+    println(solver.partTwo())
 }
