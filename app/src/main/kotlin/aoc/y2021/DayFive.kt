0e725185c66fddd672c6cd87a5fc6494cb463495
Brenna Switzer <bswitzer@brex.com>
2021-12-05 13:44:28 -0800

    Day 5 Part one

--- app/src/main/kotlin/aoc/y2021/DayFive.kt
+++ app/src/main/kotlin/aoc/y2021/DayFive.kt
@@ -1,17 +1,66 @@
 package aoc.y2021
-import aoc.lib.Resources.fileAsString
+import aoc.lib.Resources.fileAsList
+import aoc.lib.Vector
 
-class DayFive(private val input: String) {
+class DayFive(private val input: List<String>) {
 
-    fun partOne(): String {
+    fun parse(): List<Vector> {
         return input
+            .flatMap { line ->
+                line.split(" -> ").map { point ->
+                    val coords = point.split(",")
+                    Vector(coords.first().toInt(), coords.last().toInt())
+                }
+            }
+            .windowed(2, 2)
+            .flatMap { line ->
+                expandLines(line.first(), line.last())
+            }
+    }
+
+    fun expandLines(p1: Vector, p2: Vector): List<Vector> {
+        if (p1.x() == p2.x()) {
+            if (p1.y() < p2.y()) {
+                return (p1.y()..p2.y()).map { point ->
+                    Vector(p1.x(), point)
+                }
+            } else {
+                return (p1.y() downTo p2.y()).map { point ->
+                    Vector(p1.x(), point)
+                }
+            }
+        }
+        if (p1.y() == p2.y()) {
+            if (p1.x() < p2.x()) {
+                return (p1.x()..p2.x()).map { point ->
+                    Vector(point, p1.y())
+                }
+            } else {
+                return (p1.x() downTo p2.x()).map { point ->
+                    Vector(point, p1.y())
+                }
+            }
+        }
+        return emptyList()
+    }
+
+    fun partOne(): Int {
+        val vents = parse()
+        val hist = mutableMapOf<Vector, Int>()
+
+        vents.forEach { point ->
+            hist[point] = hist.getOrDefault(point, 0) + 1
+        }
+        return hist.filterValues { count ->
+            count >= 2
+        }.size
     }
 
     fun partTwo() {}
 }
 
 fun main(args: Array<String>) {
-    val input = fileAsString("day05_2021.txt")
+    val input = fileAsList("day05_2021.txt")
     val solver = DayFive(input)
     println(solver.partOne())
 }
--- app/src/test/kotlin/aoc/y2021/DayFiveTest.kt
+++ app/src/test/kotlin/aoc/y2021/DayFiveTest.kt
@@ -1,12 +1,45 @@
 package aoc.y2021
 
-import org.junit.Assert.assertTrue
+import aoc.lib.Resources.rawAsList
+import aoc.lib.Vector
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class DayFiveTest {
 
+    val input = """
+        0,9 -> 5,9
+        8,0 -> 0,8
+        9,4 -> 3,4
+        2,2 -> 2,1
+        7,0 -> 7,4
+        6,4 -> 2,0
+        0,9 -> 2,9
+        3,4 -> 1,4
+        0,0 -> 8,8
+        5,5 -> 8,2
+    """.trimIndent()
+
+    val solver = DayFive(rawAsList(input))
+
     @Test
     fun test() {
-        assertTrue(true)
+        assertEquals(solver.partOne(), 5)
+    }
+
+    @Test
+    fun expandLinesTest() {
+        val expandedY = listOf(
+            Vector(1, 1),
+            Vector(1, 2),
+            Vector(1, 3),
+        )
+        val expandedX = listOf(
+            Vector(9, 7),
+            Vector(8, 7),
+            Vector(7, 7),
+        )
+        assertEquals(expandedY, solver.expandLines(Vector(1, 1), Vector(1, 3)))
+        assertEquals(expandedX, solver.expandLines(Vector(9, 7), Vector(7, 7)))
     }
 }
