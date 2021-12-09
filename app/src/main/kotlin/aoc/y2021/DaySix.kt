dbab199378ca4ef95d01ec35effc7fa826a03e8c
Brenna Switzer <bswitzer@brex.com>
2021-12-08 20:15:33 -0800

    Day 6 part one, part two broke

--- app/src/main/kotlin/aoc/y2021/DaySix.kt
+++ app/src/main/kotlin/aoc/y2021/DaySix.kt
@@ -1,17 +1,44 @@
 package aoc.y2021
+
 import aoc.lib.Resources.fileAsString
 
 class DaySix(private val input: String) {
+    val lanternfish: LongArray = parseInput(input)
 
-    fun partOne(): String {
-        return input
+    fun partOne(): Long =
+        run(80)
+
+    fun partTwo(): Long =
+        run(256)
+
+    private fun run(days: Int): Long {
+        repeat(days) {
+            val progression = progressGeneration(lanternfish)
+            progression[6] += progression[8]
+        }
+        return lanternfish.sum()
     }
 
-    fun partTwo() {}
+    // array index is day
+    private fun progressGeneration(lanternfish: LongArray): LongArray {
+        val first = lanternfish.first()
+        lanternfish.copyInto(lanternfish, startIndex = 1)
+        lanternfish[lanternfish.lastIndex] = first
+        return lanternfish
+    }
+
+    private fun parseInput(input: String): LongArray =
+        LongArray(9).apply {
+            input
+                .split(",")
+                .map { it.toInt() }
+                .forEach { this[it] += 1L }
+        }
 }
 
 fun main(args: Array<String>) {
     val input = fileAsString("day06_2021.txt")
     val solver = DaySix(input)
-    println(solver.partOne())
+    println(solver.partOne()) //390011
+    println(solver.partTwo()) //1852978735751314
 }
--- app/src/test/kotlin/aoc/y2021/DaySixTest.kt
+++ app/src/test/kotlin/aoc/y2021/DaySixTest.kt
@@ -1,12 +1,22 @@
 package aoc.y2021
 
-import org.junit.Assert.assertTrue
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class DaySixTest {
 
+    val input = """
+        3,4,3,1,2
+    """.trimIndent()
+
     @Test
-    fun test() {
-        assertTrue(true)
+    fun testPartOne() {
+        val solver = DaySix(input)
+        assertEquals(5934, solver.partOne())
+    }
+
+    fun testPartTwo() {
+        val solver = DaySix(input)
+        assertEquals(26984457539, solver.partTwo())
     }
 }
