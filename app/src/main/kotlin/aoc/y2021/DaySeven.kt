c24b4a80f1517cfb443ed01273fe76bc7ea71a35
Brenna Switzer <bswitzer@brex.com>
2021-12-09 17:35:12 -0800

    Day 7 part one and two

--- app/src/main/kotlin/aoc/y2021/DaySeven.kt
+++ app/src/main/kotlin/aoc/y2021/DaySeven.kt
@@ -1,17 +1,53 @@
 package aoc.y2021
 import aoc.lib.Resources.fileAsString
+import java.lang.Math.abs
 
 class DaySeven(private val input: String) {
 
-    fun partOne(): String {
-       return input
+    val max = parse().maxOrNull() ?: 0
+    val min = parse().minOrNull() ?: 0
+
+    fun parse(): List<Int> {
+        return input
+            .split(",")
+            .map {
+                it.toInt()
+            }
     }
 
-    fun partTwo() {}
+    fun costOfFuel(position: Int): Int {
+        return parse().fold(0) { fuel, crab ->
+            abs(crab - position) + fuel
+        }
+    }
+
+    fun costOfExponentialFuel(position: Int): Int {
+        return parse().fold(0) { fuel, crab ->
+            val diff = abs(crab - position)
+            (1..diff).sum() + fuel
+        }
+    }
+
+    fun partOne(): Int {
+        val costs = mutableListOf<Int>()
+        (min..max).forEach { position ->
+            costs.add(costOfFuel(position))
+        }
+        return costs.minOrNull() ?: 0
+    }
+
+    fun partTwo(): Int {
+        val costs = mutableListOf<Int>()
+        (min..max).forEach { position ->
+            costs.add(costOfExponentialFuel(position))
+        }
+        return costs.minOrNull() ?: 0
+    }
 }
 
 fun main(args: Array<String>) {
     val input = fileAsString("day07_2021.txt")
     val solver = DaySeven(input)
     println(solver.partOne())
+    println(solver.partTwo())
 }
--- app/src/test/kotlin/aoc/y2021/DaySevenTest.kt
+++ app/src/test/kotlin/aoc/y2021/DaySevenTest.kt
@@ -1,12 +1,54 @@
 package aoc.y2021
 
-import org.junit.Assert.assertTrue
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class DaySevenTest {
 
+    val input = """
+        16,1,2,0,4,2,7,1,2,14
+    """.trimIndent()
+
+    val solver = DaySeven(input)
+
     @Test
-    fun test() {
-        assertTrue(true)
+    fun testP1Ex1() {
+        assertEquals(solver.costOfFuel(2), 37)
     }
+
+    @Test
+    fun testP1Ex2() {
+        assertEquals(solver.costOfFuel(1), 41)
+    }
+
+    @Test
+    fun testP1Ex3() {
+        assertEquals(solver.costOfFuel(3), 39)
+    }
+
+    @Test
+    fun testP1Ex4() {
+        assertEquals(solver.costOfFuel(10), 71)
+    }
+
+    @Test
+    fun testP1Ex5() {
+        assertEquals(37, solver.partOne())
+    }
+
+    @Test
+    fun testP2Ex1() {
+        assertEquals(168, solver.costOfExponentialFuel(5))
+    }
+
+    @Test
+    fun testP2Ex2() {
+        assertEquals(206, solver.costOfExponentialFuel(2))
+    }
+
+    @Test
+    fun testP2Ex3() {
+        assertEquals(168, solver.partTwo())
+    }
+
 }
