94b5e6dbfed6fc74973a75745535f480bce2f4dc
Brenna Switzer <bswitzer@brex.com>
2021-12-19 14:36:51 -0800

    Day 14, not working. starting over

--- app/src/main/kotlin/aoc/y2021/DayFourteen.kt
+++ app/src/main/kotlin/aoc/y2021/DayFourteen.kt
@@ -1,13 +1,70 @@
 package aoc.y2021
 import aoc.lib.Resources.fileAsString
 
+typealias Histogram = Map<String, Long>
+
 class DayFourteen(private val input: String) {
 
-    fun partOne(): String {
-       return input
+    val lastElement = 'B'
+    val template = "NNCB"
+    val start = template.toHistogram()
+
+    val rules: Map<String, String> = mapOf(
+        "CH" to "B",
+        "HH" to "N",
+        "CB" to "H",
+        "NH" to "C",
+        "HB" to "C",
+        "HC" to "B",
+        "HN" to "C",
+        "NN" to "C",
+        "BH" to "H",
+        "NC" to "B",
+        "NB" to "B",
+        "BN" to "B",
+        "BB" to "N",
+        "BC" to "B",
+        "CC" to "N",
+        "CN" to "C",
+    )
+
+    fun run(steps: Int): Long {
+        return (0..steps)
+            .fold(start) { polymer, _ -> polymer.calculate() }
+            .occurances()
+            .values
+            .sorted()
+            .let { it.last() - it.first() }
     }
 
+    fun Histogram.calculate(): Histogram {
+        val polymers = mutableMapOf<String, Long>()
+        forEach { pair, count ->
+            val element = rules.getValue(pair)
+            val inc1 = polymers.getOrDefault("${pair.first()}$element", 0L) + count
+            val inc2 = polymers.getOrDefault("$element${pair.last()}", 0L) + count
+            polymers.set("${pair.first()}$element", inc1)
+            polymers.set("$element${pair.last()}", inc2)
+        }
+        return polymers.toMap()
+    }
+
+    fun Histogram.occurances(): Map<Char, Long> {
+        return this.map { (pair, count) ->
+            pair.first() to count
+        }
+            .groupBy({ it.first }, { it.second })
+            .mapValues { it.value.sum() + if (it.key == lastElement) 1 else 0 }
+    }
+
+    fun partOne(): Long = run(10)
+
     fun partTwo() {}
+
+    fun String.toHistogram() = template.windowed(2, 1)
+        .groupingBy { it }
+        .eachCount()
+        .mapValues { it.value.toLong() }
 }
 
 fun main(args: Array<String>) {
--- app/src/test/kotlin/aoc/y2021/DayFourteenTest.kt
+++ app/src/test/kotlin/aoc/y2021/DayFourteenTest.kt
@@ -1,12 +1,36 @@
 package aoc.y2021
 
+import aoc.lib.Resources.rawAsList
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class DayFourteenTest {
 
+    val input = """
+        NNCB
+
+        CH -> B
+        HH -> N
+        CB -> H
+        NH -> C
+        HB -> C
+        HC -> B
+        HN -> C
+        NN -> C
+        BH -> H
+        NC -> B
+        NB -> B
+        BN -> B
+        BB -> N
+        BC -> B
+        CC -> N
+        CN -> C
+    """.trimIndent()
+
     @Test
     fun test() {
-        assertTrue(true)
+        val solver = DayFourteen(input)
+        assertEquals(1588, solver.partOne())
     }
 }
