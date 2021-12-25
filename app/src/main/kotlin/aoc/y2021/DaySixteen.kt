c96a23a7f8290663a9f1d08d4b54ac42c2cf2e20
bswitzer <brenna.switzer@gmail.com>
2021-12-24 20:46:09 -0800

    some notes on day 16

--- app/src/main/kotlin/aoc/y2021/DaySixteen.kt
+++ app/src/main/kotlin/aoc/y2021/DaySixteen.kt
@@ -3,7 +3,35 @@ import aoc.lib.Resources.fileAsString
 
 class DaySixteen(private val input: String) {
 
+//    0 = 0000
+//    1 = 0001
+//    2 = 0010
+//    3 = 0011
+//    4 = 0100
+//    5 = 0101
+//    6 = 0110
+//    7 = 0111
+//    8 = 1000
+//    9 = 1001
+//    A = 1010
+//    B = 1011
+//    C = 1100
+//    D = 1101
+//    E = 1110
+//    F = 1111
+
+    // a single packet at its outermost layer which itself contains many other packets.
+    // Every packet begins with a standard header:
+    //  - the first three bits encode the packet version,
+    //  - and the next three bits encode the packet type ID.
+
     fun partOne(): String {
+
+        // convert hex to binary
+        // first three bits are version
+        // next three bits are typeID
+
+        // packets with typeID 4 represent literal value
        return input
     }
 
--- app/src/test/kotlin/aoc/y2021/DaySixteenTest.kt
+++ app/src/test/kotlin/aoc/y2021/DaySixteenTest.kt
@@ -5,8 +5,41 @@ import org.junit.Test
 
 class DaySixteenTest {
 
+    val exInput = "D2FE28"
+
     @Test
     fun test() {
+        val solver = DaySixteen(exInput)
+//        110100101111111000101000
+//        VVVTTTAAAAABBBBBCCCCC
+
+        // LITERAL
+        // convert to binary should be 110100101111111000101000
+        // version 110
+        // type 100 (4), LITERAL VALUE
+        // first group - prefix + 4 bits 1-0111 (prefix one means not done)
+        // second group - prefix + 4 bits 1-1110 (prefix one means not done)
+        // third ground = prefix + 4 bits 0-0101 (prefix means last group)
+        // literal value = 0111 1110 0101
+        // convert to decimal = 2021
+
+        /* OPERATOR
+        * 00111000000000000110111101000101001010010001001000000000
+        * VVVTTTILLLLLLLLLLLLLLLAAAAAAAAAAABBBBBBBBBBBBBBBB
+        *
+        * version 001
+        * type 110 (6)
+        * length type ID 0 - next 15 bits are a number that represents the total length in bits of the sub-packets contained by this packet.
+        * length type ID 1 - next 11 bits are a number that represents the number of sub-packets immediately contained by this packet.
+        *
+        * The 15 bits labeled L (000000000011011) contain the length of the sub-packets in bits, 27.
+        *
+        * packet one
+        * 110 100 0 1010 (literal value 10)
+        *
+        * packet two
+        * 010 100 1 0001 0 0100 = 00010100 (literal value 20)
+        */
         assertTrue(true)
     }
 }
