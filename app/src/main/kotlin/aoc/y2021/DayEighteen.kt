d9923e71a2f9836d5e78efb5904ee94f1560e362
bswitzer <brenna.switzer@gmail.com>
2022-01-09 15:52:42 -0800

    Maybe explode is working? Tests pass anyway

--- app/src/main/kotlin/aoc/y2021/DayEighteen.kt
+++ app/src/main/kotlin/aoc/y2021/DayEighteen.kt
@@ -1,51 +1,64 @@
 package aoc.y2021
 
-import aoc.lib.MutableStack
 import aoc.lib.Resources.fileAsString
 
-data class SFNum(
-    val left: SFNum?,
-    val right: SFNum?,
-    val value: Int?,
-) {
-    constructor(value: Int) : this(null, null, value)
-    constructor(left: SFNum, right: SFNum) : this(left, right, null)
-
-    fun SFNum.add(other: SFNum): SFNum {
-        val added = SFNum(
-            left = this,
-            right = other
-        )
-        return added.reduced()
-    }
-
-    fun SFNum.reduced(): SFNum {
-        // do while there are no more actions to take
-        // any explodable numbers?
-        // any splitable numbers?
-        return this
-    }
-
-    // traverse to find regular numbers on left of tree?
-
-    fun traverse(depth: Int = 0): Int {
-        if (depth == 5) {
-            // this is how we know it needs to be exploded
-            return value!!
-        }
-        val left = left?.traverse(depth = depth + 1)
-        val right = right?.traverse(depth = depth + 1)
-        return 0
-    }
-}
+typealias SnailFish = List<Char>
 
 // Day 18: Snailfish
 // https://adventofcode.com/2021/day/18
 class DayEighteen(private val input: String) {
 
-    // If any pair is nested inside four pairs, the leftmost such pair explodes.
-    // If any regular number is 10 or greater, the leftmost such regular number splits.
-    val numbers = listOf<SFNum>()
+    fun add(first: SnailFish, second: SnailFish): SnailFish {
+        val str = "[" + first.joinToString("") + "," + second.joinToString("") + "]"
+        return str.toSnailFishNumber()
+    }
+
+    fun explode(number: SnailFish): SnailFish? {
+        val num = number.toMutableList()
+        var depth = 0
+        num.forEachIndexed { idx, token ->
+            when (token) {
+                '[' -> depth++
+                ']' -> depth--
+            }
+            if (depth == 5) {
+                val leftPart = num.subList(0, idx)
+                val rightPart = num.subList(idx + 5, num.size)
+                var leftValue = 0
+                var rightValue = 0
+                val leftIdx = firstIndexOfToLeft(num, idx)
+                val rightIdx = firstIndexOfToRight(num, idx + 4)
+                if (leftIdx != null) {
+                    leftValue = num[idx + 1].digitToInt() + num[leftIdx].digitToInt()
+                    num[leftIdx] = leftValue.digitToChar()
+                }
+                if (rightIdx != null) {
+                    rightValue = num[idx + 3].digitToInt() + num[rightIdx].digitToInt()
+                    num[rightIdx] = rightValue.digitToChar()
+                }
+                return leftPart + '0' + rightPart
+            }
+        }
+        return null
+    }
+
+    fun firstIndexOfToLeft(number: SnailFish, startsAt: Int): Int? {
+        (startsAt downTo 0).forEach { i ->
+            if (number[i].isDigit()) {
+                return i
+            }
+        }
+        return null
+    }
+
+    fun firstIndexOfToRight(number: SnailFish, startsAt: Int): Int? {
+        (startsAt until number.size).forEach { i ->
+            if (number[i].isDigit()) {
+                return i
+            }
+        }
+        return null
+    }
 
     fun partOne(): String {
         // result = add one and two
@@ -55,6 +68,8 @@ class DayEighteen(private val input: String) {
     }
 
     fun partTwo() {}
+
+    private fun String.toSnailFishNumber() = toList()
 }
 
 fun main(args: Array<String>) {
@@ -63,25 +78,25 @@ fun main(args: Array<String>) {
     println(solver.partOne())
 }
 
-fun String.toSFNum(): SFNum {
-    val stack = MutableStack<SFNum>()
-    val split = toCharArray()
-    split.forEach { c ->
-        when {
-            c.isDigit() -> {
-                stack.push(SFNum(value = c.digitToInt()))
-            }
-            c == ']' -> {
-                val right = stack.pop()
-                val left = stack.pop()
-                stack.push(
-                    SFNum(
-                        left = left,
-                        right = right,
-                    )
-                )
-            }
-        }
-    }
-    return stack.pop()
-}
+// fun String.toSFNum(): SFNum {
+//    val stack = MutableStack<SFNum>()
+//    val split = toCharArray()
+//    split.forEach { c ->
+//        when {
+//            c.isDigit() -> {
+//                stack.push(SFNum(value = c.digitToInt()))
+//            }
+//            c == ']' -> {
+//                val right = stack.pop()
+//                val left = stack.pop()
+//                stack.push(
+//                    SFNum(
+//                        left = left,
+//                        right = right,
+//                    )
+//                )
+//            }
+//        }
+//    }
+//    return stack.pop()
+// }
--- app/src/test/kotlin/aoc/y2021/DayEighteenTest.kt
+++ app/src/test/kotlin/aoc/y2021/DayEighteenTest.kt
@@ -5,71 +5,48 @@ import kotlin.test.assertEquals
 
 class DayEighteenTest {
 
+    val solver = DayEighteen("noop")
+
     @Test
     fun testParse() {
-        val input = "[1,2]"
-        val result = SFNum(
-            left = SFNum(value = 1),
-            right = SFNum(value = 2)
-        )
-        assertEquals(result, input.toSFNum())
+        val first = "[1,2]".toList()
+        val second = "[[3,4],5]".toList()
+        val result = "[[1,2],[[3,4],5]]".toList()
+        assertEquals(result, solver.add(first, second))
     }
 
     @Test
-    fun testParseWithNesting() {
-        val input = "[[[9,8],1],2]"
-        val result = SFNum(
-            left = SFNum(
-                left = SFNum(
-                    left = SFNum(value = 9),
-                    right = SFNum(value = 8)
-                ),
-                right = SFNum(value = 1)
-            ),
-            right = SFNum(value = 2)
-        )
-        assertEquals(result, input.toSFNum())
+    fun testExplode1() {
+        val number = "[[[[[9,8],1],2],3],4]".toList()
+        val result = "[[[[0,9],2],3],4]".toList()
+        assertEquals(result, solver.explode(number))
     }
 
     @Test
-    fun testParseWithMoarNesting() {
-        val input = "[[[9,8],1],[1,2]]"
-        val result = SFNum(
-            left = SFNum(
-                left = SFNum(
-                    left = SFNum(value = 9),
-                    right = SFNum(value = 8)
-                ),
-                right = SFNum(value = 1)
-            ),
-            right = SFNum(
-                left = SFNum(value = 1),
-                right = SFNum(value = 2)
-            )
-        )
-        assertEquals(result, input.toSFNum())
+    fun testExplode2() {
+        val number = "[7,[6,[5,[4,[3,2]]]]]".toList()
+        val result = "[7,[6,[5,[7,0]]]]".toList()
+        assertEquals(result, solver.explode(number))
     }
 
     @Test
-    fun testTraverse() {
-        val num = "[[[[[9,8],1],2],3],4]".toSFNum()
-        assertEquals(num.traverse(), 0)
+    fun testExplode3() {
+        val number = "[[6,[5,[4,[3,2]]]],1]".toList()
+        val result = "[[6,[5,[7,0]]],3]".toList()
+        assertEquals(result, solver.explode(number))
     }
 
     @Test
-    fun testExplode() {
-//        [[9,8],1]
-        val explode = SFNum(
-            left = SFNum(
-                left = SFNum(
-                    left = SFNum(value = 9),
-                    right = SFNum(value = 8)
-                ),
-                right = SFNum(value = 1)
-            ),
-            right = SFNum(value = 2)
-        )
+    fun testExplode4() {
+        val number = "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]".toList()
+        val result = "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]".toList()
+        assertEquals(result, solver.explode(number))
+    }
 
-//        assertEquals(.explode("[[[[[9,8],1],2],3],4]"), "[[[[0,9],2],3],4]")
+    @Test
+    fun testExplode5() {
+        val number = "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]".toList()
+        val result = "[[3,[2,[8,0]]],[9,[5,[7,0]]]]".toList()
+        assertEquals(result, solver.explode(number))
     }
 }
