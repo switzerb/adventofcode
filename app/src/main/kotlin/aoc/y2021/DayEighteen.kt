472731acf93ea800f63670b2c8d1481238589306
bswitzer <brenna.switzer@gmail.com>
2022-01-09 21:26:08 -0800

    Trying again with strings instead of chars

--- app/src/main/kotlin/aoc/y2021/DayEighteen.kt
+++ app/src/main/kotlin/aoc/y2021/DayEighteen.kt
@@ -4,7 +4,7 @@ import aoc.lib.Resources.fileAsList
 import kotlin.math.ceil
 import kotlin.math.floor
 
-typealias SnailFish = List<Char>
+typealias SnailFish = List<String>
 
 // Day 18: Snailfish
 // https://adventofcode.com/2021/day/18
@@ -20,17 +20,13 @@ class DayEighteen(private val input: List<String>) {
     fun split(number: SnailFish): SnailFish {
         val num = number.toMutableList()
         num.forEachIndexed { idx, token ->
-            if (
-                idx < num.size - 1 &&
-                token.isDigit() &&
-                num[idx + 1].isDigit()
-            ) {
+            val t = token.toIntOrNull()
+            if (t != null && t >= 10) {
                 val leftPart = num.subList(0, idx)
-                val rightPart = num.subList(idx + 2, num.size)
-                val value = (num[idx].toString() + num[idx + 1].toString()).toInt()
-                val left = floor((value.toDouble() / 2)).toInt()
-                val right = ceil((value.toDouble() / 2)).toInt()
-                val pair = listOf('[', left.digitToChar(), ',', right.digitToChar(), ']')
+                val rightPart = num.subList(idx + 1, num.size)
+                val left = floor((t.toDouble() / 2)).toInt()
+                val right = ceil((t.toDouble() / 2)).toInt()
+                val pair = listOf("[", left.toString(), ",", right.toString(), "]")
                 return leftPart + pair + rightPart
             }
         }
@@ -38,12 +34,12 @@ class DayEighteen(private val input: List<String>) {
     }
 
     fun explode(number: SnailFish): SnailFish {
-        var num = number.toList()
+        val num = number.toMutableList()
         var depth = 0
         num.forEachIndexed { idx, token ->
             when (token) {
-                '[' -> depth++
-                ']' -> depth--
+                "[" -> depth++
+                "]" -> depth--
             }
             if (depth == 5) {
                 var leftValue = 0
@@ -51,16 +47,14 @@ class DayEighteen(private val input: List<String>) {
                 val leftIdx = firstIndexOfToLeft(num, idx)
                 val rightIdx = firstIndexOfToRight(num, idx + 4)
                 if (leftIdx != null) {
-                    leftValue = num[idx + 1].digitToInt() + num[leftIdx].digitToInt()
-                    val adds = leftValue.toString().toList()
-                    num = num.subList(0, leftIdx) + adds + num.subList(leftIdx + 1, num.size)
+                    leftValue = num[idx + 1].toInt() + num[leftIdx].toInt()
+                    num[leftIdx] = leftValue.toString()
                 }
                 if (rightIdx != null) {
-                    rightValue = num[idx + 3].digitToInt() + num[rightIdx].digitToInt()
-                    val adds = rightValue.toString().toList()
-                    num = num.subList(0, rightIdx) + adds + num.subList(rightIdx + 1, num.size)
+                    rightValue = num[idx + 3].toInt() + num[rightIdx].toInt()
+                    num[rightIdx] = rightValue.toString()
                 }
-                return num.subList(0, idx) + '0' + num.subList(idx + 5, num.size)
+                return num.subList(0, idx) + "0" + num.subList(idx + 5, num.size)
             }
         }
         return number
@@ -68,7 +62,7 @@ class DayEighteen(private val input: List<String>) {
 
     private fun firstIndexOfToLeft(number: SnailFish, startsAt: Int): Int? {
         (startsAt downTo 0).forEach { i ->
-            if (number[i].isDigit()) {
+            if (number[i].toIntOrNull() != null) {
                 return i
             }
         }
@@ -77,7 +71,7 @@ class DayEighteen(private val input: List<String>) {
 
     private fun firstIndexOfToRight(number: SnailFish, startsAt: Int): Int? {
         (startsAt until number.size).forEach { i ->
-            if (number[i].isDigit()) {
+            if (number[i].toIntOrNull() != null) {
                 return i
             }
         }
@@ -86,34 +80,24 @@ class DayEighteen(private val input: List<String>) {
 
     fun partOne(): Int {
         var current = add(numbers[0], numbers[1])
-        println(current)
-        var again = true
-        do {
-            val init = current.size
-            do {
-                val s = current.size
-                current = explode(current)
-                println(current)
-            } while (current.size != s)
-
-            do {
-                val s = current.size
-                current = split(current)
-                println(current)
-            } while (current.size != s)
-
-            if (current.size == init) {
-                again = false
+        while (true) {
+            val size = current.size
+            current = explode(current)
+            if (current.size != size) {
+                continue
             }
-        } while (again)
-
+            current = split(current)
+            if (current.size == size) {
+                break
+            }
+        }
         println(current)
         return 0
     }
 
     fun partTwo() {}
 
-    private fun String.toSnailFishNumber() = toList()
+    private fun String.toSnailFishNumber() = split("")
 }
 
 fun main(args: Array<String>) {
--- app/src/test/kotlin/aoc/y2021/DayEighteenTest.kt
+++ app/src/test/kotlin/aoc/y2021/DayEighteenTest.kt
@@ -6,69 +6,63 @@ import kotlin.test.assertEquals
 class DayEighteenTest {
 
     val noop = DayEighteen(listOf("noop"))
+    private fun String.toSnailFishNumber() = split("")
 
     @Test
     fun testParse() {
-        val first = "[1,2]".toList()
-        val second = "[[3,4],5]".toList()
-        val result = "[[1,2],[[3,4],5]]".toList()
+        val first = "[1,2]".toSnailFishNumber()
+        val second = "[[3,4],5]".toSnailFishNumber()
+        val result = "[[1,2],[[3,4],5]]".toSnailFishNumber()
         assertEquals(result, noop.add(first, second))
     }
 
     @Test
     fun testExplode1() {
-        val number = "[[[[[9,8],1],2],3],4]".toList()
-        val result = "[[[[0,9],2],3],4]".toList()
+        val number = "[[[[[9,8],1],2],3],4]".toSnailFishNumber()
+        val result = "[[[[0,9],2],3],4]".toSnailFishNumber()
         assertEquals(result, noop.explode(number))
     }
 
     @Test
     fun testExplode2() {
-        val number = "[7,[6,[5,[4,[3,2]]]]]".toList()
-        val result = "[7,[6,[5,[7,0]]]]".toList()
+        val number = "[7,[6,[5,[4,[3,2]]]]]".toSnailFishNumber()
+        val result = "[7,[6,[5,[7,0]]]]".toSnailFishNumber()
         assertEquals(result, noop.explode(number))
     }
 
     @Test
     fun testExplode3() {
-        val number = "[[6,[5,[4,[3,2]]]],1]".toList()
-        val result = "[[6,[5,[7,0]]],3]".toList()
+        val number = "[[6,[5,[4,[3,2]]]],1]".toSnailFishNumber()
+        val result = "[[6,[5,[7,0]]],3]".toSnailFishNumber()
         assertEquals(result, noop.explode(number))
     }
 
     @Test
     fun testExplode4() {
-        val number = "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]".toList()
-        val result = "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]".toList()
+        val number = "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]".toSnailFishNumber()
+        val result = "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]".toSnailFishNumber()
         assertEquals(result, noop.explode(number))
     }
 
     @Test
     fun testExplode5() {
-        val number = "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]".toList()
-        val result = "[[3,[2,[8,0]]],[9,[5,[7,0]]]]".toList()
-        assertEquals(result, noop.explode(number))
-    }
-
-    @Test
-    fun testExplode6() {
-        val number = "[[[[0,7],4],[7,[[8,4],9]]],[1,1]]".toList()
-        val result = "[[[[0,7],4],[15,[0,13]]],[1,1]]".toList()
+        val number = "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]".toSnailFishNumber()
+        val result = "[[3,[2,[8,0]]],[9,[5,[7,0]]]]".toSnailFishNumber()
         assertEquals(result, noop.explode(number))
     }
 
     @Test
     fun testSplit1() {
-        val number = "[[[[0,7],4],[15,[0,13]]],[1,1]]".toList()
-        val result = "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]".toList()
-        assertEquals(result, noop.split(number))
+        val number = "[[[[0,7],4],[".split("") + listOf("15", ",", "[", "0", ",", "13") + "]]],[1,1]]".split("")
+        val result = "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"
+        assertEquals(result, noop.split(number).joinToString(""))
     }
 
     @Test
     fun testSplit2() {
-        val number = "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]".toList()
-        val result = "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]".toList()
-        assertEquals(result, noop.split(number))
+        val number = "[[[[0,7],4],[[7,8],[0,".split("") + listOf("13") + "]]],[1,1]]".split("")
+        val result = "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]"
+        assertEquals(result, noop.split(number).joinToString(""))
     }
 
     @Test
