76d33cb512601fb79efcce094557f6b808c8ad76
bswitzer <brenna.switzer@gmail.com>
2022-12-11 15:31:18 -0800

    Day 11 solution

--- app/src/main/kotlin/aoc/y2022/DayEleven.kt
+++ app/src/main/kotlin/aoc/y2022/DayEleven.kt
@@ -1,55 +1,155 @@
 package aoc.y2022
 
-typealias Item = Int
+typealias Item = Long
 
 class Monkey(
-    val id: Int,
-    val items: List<Item>,
-    val divider: Int,
-    val worryOperation: (worry: Int) -> Int,
+    val items: MutableList<Item>,
+    val divider: Long,
+    val worryOperation: (worry: Long) -> Long,
     val monkeyTId: Int,
     val monkeyFId: Int
 ) {
-    private var itemCounter = 0
-    val holding = items.toMutableList()
+    private var itemCounter = 0L
+
+    fun getInspectedCount() = itemCounter
 
     // while there is anything in the list, throw until holding nothing
-    fun throwTo(): Int {
-        val item = holding.first()
+    fun takeTurn(
+        withRelief: Boolean,
+        mod: Long?
+    ): List<Pair<Int, Item>> {
+        val monkeys = buildList {
+            for (item in items) {
+                add(throwTo(item, withRelief, mod))
+            }
+        }
+        items.clear()
+        return monkeys
+    }
+
+    fun throwTo(
+        item: Item,
+        withRelief: Boolean,
+        mod: Long?
+    ): Pair<Int, Item> {
         itemCounter++
-        val newWorries = worryOperation(item).floorDiv(3)
-        holding.remove(item)
-        if (newWorries % this.divider == 0) {
-            return monkeyTId
+
+        var newWorries = if (mod == null) worryOperation(item) else worryOperation(item % mod)
+
+        if (withRelief) {
+            newWorries = newWorries.floorDiv(3)
+        }
+        if (newWorries % this.divider == 0L) {
+            return Pair(monkeyTId, newWorries)
         } else {
-            return monkeyFId
+            return Pair(monkeyFId, newWorries)
         }
     }
 }
 
-class DayEleven(private val monkeys: List<Monkey>) {
+class DayEleven(val monkeys: List<Monkey>) {
 
 //    val monkeys = buildList {
-//
 //    }
 
-    fun round() {
+    fun round(
+        withRelief: Boolean = true,
+        mod: Long? = null
+    ) {
         for (id in monkeys.indices) {
-            val next = monkeys[id].throwTo()
-            println(monkeys[id].id)
+            val throwsTo = monkeys[id].takeTurn(withRelief, mod)
+            for (next in throwsTo) {
+                monkeys[next.first].items.add(next.second)
+            }
         }
     }
 
-    fun partOne(): Int {
-        round()
-        return 0
+    fun getMonkeyBusiness(): Long = monkeys
+        .map(Monkey::getInspectedCount)
+        .sortedDescending()
+        .take(2)
+        .reduce { a, item -> a * item }
+
+    fun partOne(rounds: Int = 20): Long {
+        repeat(rounds) {
+            round()
+        }
+        return getMonkeyBusiness()
     }
 
-    fun partTwo() {}
+    fun partTwo(rounds: Int = 10000): Long {
+        val mod = monkeys.map {
+            it.divider
+        }.reduce { a, n -> a * n }
+
+        for (i in 1..rounds) {
+            round(withRelief = false, mod = mod)
+        }
+        return getMonkeyBusiness()
+    }
 }
 
 fun main(args: Array<String>) {
+    val monkeys = listOf(
+        Monkey(
+            items = mutableListOf(91, 66),
+            worryOperation = { w -> w * 13 },
+            divider = 19,
+            monkeyTId = 6,
+            monkeyFId = 2
+        ),
+        Monkey(
+            items = mutableListOf(78, 97, 59),
+            worryOperation = { w -> w + 7 },
+            divider = 5,
+            monkeyTId = 0,
+            monkeyFId = 3
+        ),
+        Monkey(
+            items = mutableListOf(57, 59, 97, 84, 72, 83, 56, 76),
+            worryOperation = { w -> w + 6 },
+            divider = 11,
+            monkeyTId = 5,
+            monkeyFId = 7
+        ),
+        Monkey(
+            items = mutableListOf(81, 78, 70, 58, 84),
+            worryOperation = { w -> w + 5 },
+            divider = 17,
+            monkeyTId = 6,
+            monkeyFId = 0
+        ),
+        Monkey(
+            items = mutableListOf(60),
+            worryOperation = { w -> w + 8 },
+            divider = 7,
+            monkeyTId = 1,
+            monkeyFId = 3
+        ),
+        Monkey(
+            items = mutableListOf(57, 69, 63, 75, 62, 77, 72),
+            worryOperation = { w -> w * 5 },
+            divider = 13,
+            monkeyTId = 7,
+            monkeyFId = 4
+        ),
+        Monkey(
+            items = mutableListOf(73, 66, 86, 79, 98, 87),
+            worryOperation = { w -> w * w },
+            divider = 3,
+            monkeyTId = 5,
+            monkeyFId = 2
+        ),
+        Monkey(
+            items = mutableListOf(95, 89, 63, 67),
+            worryOperation = { w -> w + 2 },
+            divider = 2,
+            monkeyTId = 1,
+            monkeyFId = 4
+        )
+    )
 //    val input = fileAsString("2022/day11_2022.txt")
-//    val solver = DayEleven(input)
+    val solver = DayEleven(monkeys)
 //    println(solver.partOne())
+    println(solver.partTwo())
 }
--- app/src/test/kotlin/aoc/y2022/DayElevenTest.kt
+++ app/src/test/kotlin/aoc/y2022/DayElevenTest.kt
@@ -7,42 +7,61 @@ class DayElevenTest {
 
     val input = listOf(
         Monkey(
-            id = 0,
-            items = listOf(79, 98),
+            items = mutableListOf(79, 98),
             worryOperation = { w -> w * 19 },
             divider = 23,
             monkeyTId = 2,
             monkeyFId = 3
         ),
         Monkey(
-            id = 1,
-            items = listOf(54, 65, 75, 74),
+            items = mutableListOf(54, 65, 75, 74),
             worryOperation = { w -> w + 6 },
             divider = 19,
             monkeyTId = 2,
             monkeyFId = 0
         ),
         Monkey(
-            id = 2,
-            items = listOf(79, 60, 97),
+            items = mutableListOf(79, 60, 97),
             divider = 13,
             worryOperation = { w -> w * w },
             monkeyTId = 1,
             monkeyFId = 3
         ),
         Monkey(
-            id = 3,
-            items = listOf(74),
+            items = mutableListOf(74),
             divider = 17,
             worryOperation = { w -> w + 3 },
             monkeyTId = 0,
             monkeyFId = 1
         )
     )
+    val solver = DayEleven(input)
 
     @Test
-    fun test() {
-        val solver = DayEleven(input)
-        assertEquals(0, solver.partOne())
+    fun test_p1e1() {
+        assertEquals(10605, solver.partOne())
+    }
+
+    @Test
+    fun test_p2e1() {
+        assertEquals(2713310158, solver.partTwo())
+    }
+
+    @Test
+    fun t2_p2() {
+        solver.partTwo(20)
+        assertEquals(99, solver.monkeys[0].getInspectedCount())
+        assertEquals(97, solver.monkeys[1].getInspectedCount())
+        assertEquals(8, solver.monkeys[2].getInspectedCount())
+        assertEquals(103, solver.monkeys[3].getInspectedCount())
+    }
+
+    @Test
+    fun t3_p2() {
+        solver.partTwo(1000)
+        assertEquals(5204, solver.monkeys[0].getInspectedCount())
+        assertEquals(4792, solver.monkeys[1].getInspectedCount())
+        assertEquals(199, solver.monkeys[2].getInspectedCount())
+        assertEquals(5192, solver.monkeys[3].getInspectedCount())
     }
 }
