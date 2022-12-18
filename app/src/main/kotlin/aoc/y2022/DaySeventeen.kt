e66816d691e39d1ad82d7fd51f32a77cf070dc7e
bswitzer <brenna.switzer@gmail.com>
2022-12-18 13:47:52 -0800

    dear god. part two day seventeen. i think i had a stroke on this one.

--- app/src/main/kotlin/aoc/y2022/DaySeventeen.kt
+++ app/src/main/kotlin/aoc/y2022/DaySeventeen.kt
@@ -20,6 +20,10 @@ enum class Shape {
     SQUARE
 }
 
+data class Cache(val topography: List<Int>, val blocks: Int, val jets: Int)
+
+data class Meta(val totalBlocks: Int, val height: Int)
+
 data class Rock(
     val shape: Shape,
     val coords: List<Position>,
@@ -41,6 +45,9 @@ data class Rock(
 
 class DaySeventeen(private val input: String) {
     val atRest: Grid2D = Grid2D()
+    var height = 0
+    var rockIdx = 0
+    var jetIdx = 0
 
     private val rockSequence = listOf(
         Rock(
@@ -96,49 +103,77 @@ class DaySeventeen(private val input: String) {
             append('\n')
         }
 
-    fun partOne(rounds: Int): Int {
-        var rockIdx = 0
-        var jetIdx = 0
-        var height = 0
-
-        repeat(rounds) {
-            val nextRock = rockSequence[rockIdx++ % Shape.values().size]
-            var rock = nextRock.move(Position(2, height - 3))
-            while (true) {
-                val motion = jets[jetIdx++ % jets.size]
-
-                val pushed = when (motion) {
-                    '<' -> rock.left()
-                    '>' -> rock.right()
-                    else -> throw UnsupportedOperationException("Motion not supported")
-                }
-                if (couldMove(pushed)) rock = pushed
-//                println("Moves $motion...")
-//                println(draw(rock))
-                val dropped = rock.down()
-                if (couldMove(dropped)) {
-                    rock = dropped
-//                    println("Drops...")
-//                    println(draw(rock))
-                } else {
-                    atRest.addTo(rock.coords.toList())
-                    height = if (height == 0) -rock.tall else atRest.minY() - 1
-//                    println("At rest...")
-//                    println(draw(rock))
-                    break
-                }
+    private fun topography(coords: List<Position>): List<Int> =
+        coords.groupBy { it.x }
+            .entries
+            .sortedBy { it.key }
+            .map { pointList -> pointList.value.minByOrNull { p -> p.y } }
+            .let {
+                val top = atRest.minY()
+                it.map { p -> top - p!!.y }
             }
+
+    private fun calculateHeight(): Long {
+        val rounds = 1000000000000L - 1
+        val visited: MutableMap<Cache, Meta> = mutableMapOf()
+        while (true) {
+            run()
+            val cache = Cache(
+                topography(atRest.grid),
+                rockIdx % rockSequence.size,
+                jetIdx % jets.size
+            )
+            if (cache in visited) {
+                val (startBlocks, startHeight) = visited.getValue(cache)
+                val blocks = rockIdx - 1L - startBlocks
+                val totalLoops = (rounds - startBlocks) / blocks
+                val remaining = (rounds - startBlocks) - (totalLoops * blocks)
+                val delta = abs(height) - startHeight
+                repeat(remaining.toInt()) {
+                    run()
+                }
+                return abs(height) + (delta * (totalLoops - 1))
+            }
+            visited[cache] = Meta(rockIdx - 1, abs(height))
+        }
+    }
+
+    private fun run() {
+        val nextRock = rockSequence[rockIdx++ % Shape.values().size]
+        var rock = nextRock.move(Position(2, height - 3))
+        while (true) {
+            val motion = jets[jetIdx++ % jets.size]
+
+            val pushed = when (motion) {
+                '<' -> rock.left()
+                '>' -> rock.right()
+                else -> throw UnsupportedOperationException("Motion not supported")
+            }
+            if (couldMove(pushed)) rock = pushed
+            val dropped = rock.down()
+            if (couldMove(dropped)) {
+                rock = dropped
+            } else {
+                atRest.addTo(rock.coords.toList())
+                height = if (height == 0) -rock.tall else atRest.minY() - 1
+                break
+            }
+        }
+    }
+
+    fun partOne(rounds: Int): Int {
+        repeat(rounds) {
+            run()
         }
         return abs(height)
     }
 
-    fun partTwo(): Long {
-        return 0L
-    }
+    fun partTwo(): Long = calculateHeight()
 }
 
 fun main(args: Array<String>) {
     val input = fileAsString("2022/day17_2022.txt")
     val solver = DaySeventeen(input)
     println(solver.partOne(rounds = 2022)) // 3147
+    println(solver.partTwo())
 }
