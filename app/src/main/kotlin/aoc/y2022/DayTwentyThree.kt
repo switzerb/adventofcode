1e39dadeb9ac14237a142794c63aa3b3fa2bbc67
bswitzer <brenna.switzer@gmail.com>
2023-01-07 15:51:43 -0800

    Day 23 WIP

--- app/src/main/kotlin/aoc/y2022/DayTwentyThree.kt
+++ app/src/main/kotlin/aoc/y2022/DayTwentyThree.kt
@@ -1,6 +1,5 @@
 package aoc.y2022
 
-import aoc.lib.Direction
 import aoc.lib.Grid2D
 import aoc.lib.Position
 import aoc.lib.Resources.fileAsString
@@ -9,70 +8,77 @@ typealias Elf = Position
 
 class DayTwentyThree(private val input: String) {
 
-    val orderedDirections = listOf(Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST)
-    var orderIdx = 0
+    val elves: List<Elf> = input
+        .split("\n")
+        .flatMapIndexed { y, row ->
+            row.mapIndexedNotNull { x, char ->
+                if (char == '#') Elf(x, y) else null
+            }
+        }.toList()
 
-    val directionSet: Map<Direction, Set<Position>> = mapOf(
-        Direction.NORTH to setOf(Position(0, -1), Position(-1, -1), Position(1, -1)),
-        Direction.SOUTH to setOf(Position(0, 1), Position(-1, 1), Position(1, 1)),
-        Direction.WEST to setOf(Position(-1, 0), Position(-1, -1), Position(-1, 1)),
-        Direction.EAST to setOf(Position(1, 0), Position(1, -1), Position(1, 1))
-    )
+    val field = Grid2D(elves)
 
-    // five elves from example
-    val field = Grid2D(
+    private val nexts: List<List<Elf>> = nextDestinations()
+
+    private fun nextDestinations(): List<List<Elf>> =
         listOf(
-            Elf(0, 0),
-            Elf(1, 0),
-            Elf(0, 1),
-            Elf(0, 3),
-            Elf(1, 3)
+            listOf(Position(0, -1), Position(-1, -1), Position(1, -1)), // N
+            listOf(Position(0, 1), Position(-1, 1), Position(1, 1)), // S
+            listOf(Position(-1, 0), Position(-1, -1), Position(-1, 1)), // W
+            listOf(Position(1, 0), Position(1, -1), Position(1, 1)) // E
         )
-    )
 
-    fun proposeMove(elf: Elf): Direction? {
-        val adjacent = elf.nextToInclusive()
+    fun round(elves: List<Elf>, step: Int): List<Elf> {
+        val nextPositions = mutableListOf<Elf>()
+        nextPositions.addAll(elves)
+        val movers: Map<Elf, Elf> = elves
+            .filter { elf -> elf.nextToInclusive().any { it in elves } }
+            .mapNotNull { elf ->
+                nexts.indices.map { direction -> nexts[(step + direction) % 4] }
+                    .firstNotNullOfOrNull { offsets ->
+                        if (offsets.none { offset -> (elf + offset) in elves }) elf to (elf + offsets.first())
+                        else null
+                    }
+            }.toMap()
 
-        // if there are no elves adjacent, do nothing
-        if (field.grid.any { it in adjacent }) {
-            return null
+        val targets = movers
+            .values
+            .groupingBy { it }
+            .eachCount()
+            .filter { it.value == 1 }
+            .keys
 
-            // otherwise look in order and propose the first step where there isn't another elf
-        } else {
-            for (i in 0..3) {
-                val idx = ((orderIdx + i) % orderedDirections.size)
-                val direction = orderedDirections[idx]
-                if (directionSet[direction]!!.none { pos -> pos + elf in field.grid }) {
-                    return direction
-                }
+        movers
+            .filter { (_, target) -> target in targets }
+            .forEach { (source, target) ->
+                nextPositions.remove(source)
+                nextPositions.add(target)
             }
-        }
-        return null
+        return nextPositions
     }
 
-    fun round() {
-        val proposals = mutableListOf<Direction>()
-        for (elf in field.grid) {
-            val dir = proposeMove(elf)
-            if (dir != null) {
-                proposals.add(dir)
-            }
-        }
-        // all elves make a proposal to move
-        // get adjacent ground if there is an elf in it
-        // if there are
-        orderIdx++
+    fun partOne(): Int {
+        val locations = (0 until 10)
+            .fold(elves) { positions, step -> round(positions, step) }
+        val gridSize =
+            ((locations.maxOf { it.x } - locations.minOf { it.x }) + 1) * ((locations.maxOf { it.y } - locations.minOf { it.y }) + 1)
+        return gridSize - locations.size
     }
 
-    fun partOne(): String {
-        return input
+    fun partTwo(): Int {
+        var thisTurn = elves
+        var roundId = 0
+        do {
+            val previousTurn = thisTurn
+            thisTurn = round(previousTurn, roundId++)
+        } while (previousTurn != thisTurn)
+        return roundId
     }
-
-    fun partTwo() {}
 }
 
 fun main(args: Array<String>) {
     val input = fileAsString("2022/day23_2022.txt")
     val solver = DayTwentyThree(input)
-    println(solver.partOne())
+//    println(solver.partOne())
+    println(solver.partTwo())
 }
